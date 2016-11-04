f6e5f5a58f4a753c5116e6639d87e54490831c34
Abhishek Deep Nigam <adn5327@gmail.com>
2016-11-03 23:51:44 -0500

    beginning api layer on database

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,20 @@
 package com.company;
-import com.company.metric;
+
 import org.rocksdb.RocksDB;
 import org.rocksdb.Options;
 import org.rocksdb.RocksDBException;
+
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) throws java.io.IOException{
-
-        // Load Rocks
-        RocksDB.loadLibrary();
-        Options options = new Options().setCreateIfMissing(true);
-        RocksDB db = null;
-        try {
-            db = RocksDB.open(options, "./db");
-        } catch (RocksDBException e) {
-            System.out.println(e);
-        }
-
+    public static void main(String[] args) {
+        RocksConnector connector = new RocksConnector("db.test");
 
         // File IO
         try (BufferedReader br = new BufferedReader(new FileReader("./logs/metrics_sample.txt"))) {
@@ -30,12 +24,19 @@ public class Main {
             }
         } catch (FileNotFoundException e) {
             System.out.println(e);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
+        Metric thing = new Metric("2016-11-03 15:45:05,238 74395    1478205905\tlocalhost:6703\t  8:exclaim1   \t__emit-count           \t{default=960}");
+        System.out.println(thing.serialize());
+        connector.insert(thing);
+        List<String> x = connector.scan("74395");
 
-        // Test metric object
-        //metric thing = new metric("2016-11-03 15:45:05,238 74395    1478205905\tlocalhost:6703\t  8:exclaim1   \t__emit-count           \t{default=960}");
-        //System.out.println(thing.datestamp);
+        for(String each : x)
+        {
+            System.out.println(each);
+        }
 
     }
 }
---
+++ src/com/company/RocksConnector.java
@@ -0,0 +1,65 @@
+package com.company;
+
+import java.lang.String;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.rocksdb.RocksDB;
+import org.rocksdb.Options;
+import org.rocksdb.RocksDBException;
+import org.rocksdb.RocksIterator;
+
+/**
+ * Created by adn5327 on 11/3/16.
+ */
+public class RocksConnector {
+
+    private RocksDB db;
+    public RocksConnector(String dbfilename)
+    {
+        RocksDB.loadLibrary();
+        // the Options class contains a set of configurable DB options
+        // that determines the behavior of a database.
+        Options options = new Options().setCreateIfMissing(true);
+        this.db = null;
+        try {
+            // a factory method that returns a RocksDB instance
+            this.db = RocksDB.open(options, dbfilename);
+            // do something
+        } catch (RocksDBException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void insert(Metric m)
+    {
+        try
+        {
+            this.db.put(m.serialize().getBytes(), m.getValue().getBytes());
+        }
+        catch(RocksDBException e)
+        {
+            System.out.println("oh no!");
+        }
+    }
+
+    public void remove()
+    {
+
+    }
+
+    public List<String> scan(String prefix)
+    {
+        List<String> result = new ArrayList<String>();
+        RocksIterator iterator = this.db.newIterator();
+        for (iterator.seek(prefix.getBytes()); iterator.isValid(); iterator.next()) {
+            String key = new String(iterator.key());
+            if (key.startsWith(prefix)) {
+                result.add(String.format("%s", new String(iterator.key())));
+            }
+        }
+        return result;
+    }
+
+}
